f4e5e7a59fab65039e0078bbb66de92f02f4e842
Tim Waterson <tim@waterson.co>
2015-11-19 22:03:32 +0000

    Add permissions support

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User
   property :id, Serial
   property :name, String, required: true, length: 150
   property :email, String, required: true, length: 150
+  property :permissions, Json, required: false, default: [], lazy: false
+
+  def has_permission?(permission)
+    !permissions.nil? && permissions.include?(permission.to_s)
+  end
 end
