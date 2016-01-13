c52335a348164a9c6dd1f38c9efc4ccc270c3358
a-random-oracle <ra728@york.ac.uk>
2016-01-13 19:37:00 +0000

    Monkey-patched pundit to remove rails-specific attributes and add block handling.

--- app/routes/init.rb
+++ app/routes/init.rb
@@ -1,2 +1,29 @@
 require_relative 'auth'
 require_relative 'main'
+require 'pundit'
+
+class Flippd < Sinatra::Application
+  include Pundit
+  
+  def Pundit.authorize(user, record, query, &block)
+    policy = policy!(user, record)
+    
+    authorized = policy.public_send(query)
+    if authorized and block
+      yield
+    elsif not authorized and not block
+      raise NotAuthorizedError.new(query: query, record: record, policy: policy)
+    end
+    
+    true
+  end
+  
+  def authorize(record, query, &block)
+    @_pundit_policy_authorized = true
+    Pundit.authorize(pundit_user, record, query, &block)
+  end
+
+  def pundit_user
+    @user
+  end
+end
