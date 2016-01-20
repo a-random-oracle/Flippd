9b50508fca10fea4e0c66b50bb81dd1b7ae1bc2d
a-random-oracle <ra728@york.ac.uk>
2016-01-20 22:42:19 +0000

    Slight modifications to User to clarify attribute and method names.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,21 @@ class User
 
   def self.from_oauth(oauth_data)
     # Fetch authorisation data (from the URL in the project's .env file)
-    users = JSON.load(open(ENV['CONFIG_URL'] + "users.json")) rescue {}
-    permissions = JSON.load(open(ENV['CONFIG_URL'] + "permissions.json")) rescue {}
+    users_json = JSON.load(open(ENV['CONFIG_URL'] + "users.json")) rescue {}
+    permissions_json = JSON.load(open(ENV['CONFIG_URL'] + "permissions.json")) rescue {}
 
     email = oauth_data.info.email
     name = oauth_data.info.name
-    group = users.keys.find{ |k| users[k].include?(email) } || "guest"
-    permissions = permissions[group] || []
+    group = users_json.keys.find{ |k| users_json[k].include?(email) } || "guest"
+    permissions = permissions_json[group] || []
 
     user = self.first_or_new({ :email => email }, { :name => name, :permissions => permissions })
-    user.update_attributes({ :name => name, :permissions => permissions })
+    user.update_specific_attributes({ :name => name, :permissions => permissions })
 
     user
   end
 
-  def update_attributes(attributes)
+  def update_specific_attributes(attributes)
     attributes.each do |attribute, value|
       if attribute_get(attribute) != value then
         attribute_set(attribute, value)
