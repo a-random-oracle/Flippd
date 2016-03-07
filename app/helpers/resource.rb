034904b2f8c2c0fd0286417b628f2aa19946bbf3
a-random-oracle <ra728@york.ac.uk>
2016-03-07 22:59:33 +0000

    Moved resource loader selection into the resource class.

--- app/helpers/resource.rb
+++ app/helpers/resource.rb
@@ -6,4 +6,18 @@ class Resource
     @location = location
     @type = type
   end
+  
+  def load()
+    loader = find_loader()
+    loader.load(self)
+  end
+  
+  private
+    
+  def find_loader()
+    ResourceLoaders.constants.each do |loader_symbol|
+      loader = ResourceLoaders.const_get(loader_symbol)
+      return loader if loader::LOADER_FOR == @type
+    end
+  end
 end
