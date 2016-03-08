08cda21dab4b065fdd8ef0a24e9858f16b364f38
a-random-oracle <ra728@york.ac.uk>
2016-03-08 16:57:36 +0000

    Also moving question feedback out into a gem as it is a dependancy for the question types.

--- app/models/question-feedback.rb
+++
@@ -1,9 +0,0 @@
-QuestionFeedback = Struct.new(:correct?, :feedback) do
-  def to_json(*a)
-    to_h.to_json(*a)
-  end
-
-  def self.from_json(json)
-    self.new(json['correct?'], json['feedback'])
-  end
-end
