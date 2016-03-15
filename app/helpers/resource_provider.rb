89830b226e563f4ff3ea72798eadfa918cc4ad07
a-random-oracle <ra728@york.ac.uk>
2016-03-15 22:07:18 +0000

    Added defaults to revert to when resource validation fails.

--- app/helpers/resource_provider.rb
+++ app/helpers/resource_provider.rb
@@ -7,38 +7,37 @@ class ResourceProvider
     
     def get_index(&block)
       index = ERBLoader.load(INDEX)
-      validate_resource(ERBValidator, index, &block)
+      validate_resource(ERBValidator, index, :not_found, &block)
     end
     
     def get_module(&block)
       mod = JSONLoader.load(MODULE)
-      validate_resource(ModuleValidator, mod, &block)
+      validate_resource(ModuleValidator, mod, {}, &block)
     end
     
     def get_phases(&block)
-      mod = JSONLoader.load(MODULE)
-      
-      if validate_resource(ModuleValidator, mod, &block)
-        phases = mod['phases']
-        validate_resource(PhasesValidator, phases, &block)
-      end
+      mod = get_module
+      phases = mod['phases'] if mod.key? 'phases'
+      validate_resource(PhasesValidator, phases, {}, &block)
     end
     
     def get_users(&block)
       users = JSONLoader.load(USERS)
-      validate_resource(UsersValidator, users, &block)
+      validate_resource(UsersValidator, users, {}, &block)
     end
     
     def get_permissions(&block)
       permissions = JSONLoader.load(PERMISSIONS)
-      validate_resource(PermissionsValidator, permissions, &block)
+      validate_resource(PermissionsValidator, permissions, {}, &block)
     end
     
     private
     
-    def validate_resource(validator, resource, &block)
+    def validate_resource(validator, resource, default=nil, &block)
       if resource and validator.validate(resource)
         handle_possible_block(resource, &block)
+      else
+        handle_possible_block(default, &block)
       end
     end
     
