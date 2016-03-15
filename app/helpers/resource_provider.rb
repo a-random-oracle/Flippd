736a5b9e3f7e3176381056a604dae41f6f53d296
a-random-oracle <ra728@york.ac.uk>
2016-03-15 21:54:12 +0000

    Validation failures can now be handled.

--- app/helpers/resource_provider.rb
+++ app/helpers/resource_provider.rb
@@ -5,29 +5,49 @@ class ResourceProvider
     USERS       = ENV['CONFIG_URL'] + 'users.json'
     PERMISSIONS = ENV['CONFIG_URL'] + 'permissions.json'
     
-    def get_index()
+    def get_index(&block)
       index = ERBLoader.load(INDEX)
-      index if ERBValidator.validate(index)
+      validate_resource(ERBValidator, index, &block)
     end
     
-    def get_module()
+    def get_module(&block)
       mod = JSONLoader.load(MODULE)
-      mod if ModuleValidator.validate(mod)
+      validate_resource(ModuleValidator, mod, &block)
     end
     
-    def get_phases()
-      phases = get_module()['phases']
-      phases if PhasesValidator.validate(phases)
+    def get_phases(&block)
+      mod = JSONLoader.load(MODULE)
+      
+      if validate_resource(ModuleValidator, mod, &block)
+        phases = mod['phases']
+        validate_resource(PhasesValidator, phases, &block)
+      end
     end
     
-    def get_users()
+    def get_users(&block)
       users = JSONLoader.load(USERS)
-      users if UsersValidator.validate(users)
+      validate_resource(UsersValidator, users, &block)
     end
     
-    def get_permissions()
+    def get_permissions(&block)
       permissions = JSONLoader.load(PERMISSIONS)
-      permissions if PermissionsValidator.validate(permissions)
+      validate_resource(PermissionsValidator, permissions, &block)
+    end
+    
+    private
+    
+    def validate_resource(validator, resource, &block)
+      if resource and validator.validate(resource)
+        handle_possible_block(resource, &block)
+      end
+    end
+    
+    def handle_possible_block(resource)
+      if block_given?
+        yield resource
+      else
+        resource
+      end
     end
   end
 end
