0e02d15467f10ffcaa35ef52f1c22e9e17c3c55a
a-random-oracle <ra728@york.ac.uk>
2016-04-08 17:42:45 +0100

    Required resource validators.

--- app/flippd.rb
+++ app/flippd.rb
@@ -7,8 +7,9 @@ require 'sinatra'
 require 'sinatra/multi_route'
 require 'rack-flash'
 
-Dir[File.join(File.dirname(__FILE__), 'helpers', '*.rb')].each { |file| require file }
 Dir[File.join(File.dirname(__FILE__), 'helpers', 'resource_loaders', '*.rb')].each { |file| require file }
+Dir[File.join(File.dirname(__FILE__), 'helpers', 'resource_validators', '*.rb')].each { |file| require file }
+Dir[File.join(File.dirname(__FILE__), 'helpers', '**', '*.rb')].each { |file| require file }
 
 class Flippd < Sinatra::Application
   register Sinatra::MultiRoute
