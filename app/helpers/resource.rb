e170c6d1dc15627d8e47ef77f0dd18cffd9acf9a
a-random-oracle <ra728@york.ac.uk>
2016-04-08 17:58:56 +0100

    Added a validator field to resources.

--- app/helpers/resource.rb
+++ app/helpers/resource.rb
@@ -1,9 +1,11 @@
 class Resource
   attr_reader :location
   attr_reader :type
+  attr_reader :validator
 
-  def initialize(location, type)
+  def initialize(location, type, validator=nil)
     @location = location
     @type = type
+    @validator = validator
   end
 end
--- app/helpers/resources.rb
+++ app/helpers/resources.rb
@@ -1,7 +1,7 @@
 class Resources
-  INDEX_PAGE  = Resource.new(ENV['CONFIG_URL'] + 'index.erb', :erb)
-  MODULE      = Resource.new(ENV['CONFIG_URL'] + 'module.json', :json)
-  PHASES      = Resource.new(ENV['CONFIG_URL'] + 'module.json', :json)
-  USERS       = Resource.new(ENV['CONFIG_URL'] + 'users.json', :json)
-  PERMISSIONS = Resource.new(ENV['CONFIG_URL'] + 'permissions.json', :json)
+  INDEX_PAGE  = Resource.new(ENV['CONFIG_URL'] + 'index.erb', :erb, ERBValidator)
+  MODULE      = Resource.new(ENV['CONFIG_URL'] + 'module.json', :json, ModuleValidator)
+  PHASES      = Resource.new(ENV['CONFIG_URL'] + 'module.json', :json, PhasesValidator)
+  USERS       = Resource.new(ENV['CONFIG_URL'] + 'users.json', :json, UsersValidator)
+  PERMISSIONS = Resource.new(ENV['CONFIG_URL'] + 'permissions.json', :json, PermissionsValidator)
 end
