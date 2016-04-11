a3fd5335061a14d28c35e657dfd86bd073b35b94
a-random-oracle <ra728@york.ac.uk>
2016-04-12 00:21:20 +0100

    Resources are now validated as they are loaded.

--- app/helpers/resource.rb
+++ app/helpers/resource.rb
@@ -1,15 +1,28 @@
 class Resource
   attr_reader :location
   attr_reader :loader
+  attr_reader :validator
 
-  def initialize(location, loader)
+  def initialize(location, loader, validator=nil)
     @location = location
     @loader = loader
+    @validator = validator
   end
   
   def load()
-    @loader.load(self)
+    loaded_resource = @loader.load(self)
+    if validate(loaded_resource)
+      loaded_resource
+    else
+      raise "The resource at #{@location} could not be loaded."
+    end
   rescue
     nil
+  end
+  
+  private
+  
+  def validate(loaded_resource)
+    @validator ? @validator.validate(loaded_resource) : true
   end
 end
