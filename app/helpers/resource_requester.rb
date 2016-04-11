1e72422551b4527e3fb96ade674370a285ac21b9
a-random-oracle <ra728@york.ac.uk>
2016-04-11 22:53:46 +0100

    Errors during resource loading are now caught.

--- app/helpers/resource_requester.rb
+++ app/helpers/resource_requester.rb
@@ -1,11 +1,17 @@
 class ResourceRequester
   class << self
     def request(resource)
-      loaded_resource = resource.loader.load(resource)
+      loaded_resource = load_resource(resource)
       validate_resource(resource, loaded_resource) ? loaded_resource : resource.default
     end
 
     private
+
+    def load_resource(resource)
+      resource.loader.load(resource)
+    rescue
+      nil
+    end
 
     def validate_resource(resource, loaded_resource)
       resource.validator ? resource.validator.validate(loaded_resource) : true
