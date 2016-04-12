c85b124496bc76db5e2d7fa97d5a304ae2eb308f
a-random-oracle <ra728@york.ac.uk>
2016-04-12 12:54:33 +0100

    Implemented resource caching.

--- app/helpers/resource.rb
+++ app/helpers/resource.rb
@@ -2,18 +2,25 @@ class Resource
   attr_reader :loader
   attr_reader :validator
   attr_reader :default
+  attr_reader :cached_value
 
   def initialize(loader, validator=nil, default=nil)
     @loader = loader
-    @default = default
     @validator = validator
+    @default = default
+    @cached_value = :not_set
   end
   
   def load
-    loaded_resource = @loader.load(self)
-    validate(loaded_resource) ? loaded_resource : @default
-  rescue
-    @default
+    if @cached_value == :not_set
+      begin
+        loaded_resource = @loader.load(self)
+        @cached_value = validate(loaded_resource) ? loaded_resource : @default
+      rescue
+        @cached_value = @default
+      end
+    end
+    @cached_value
   end
   
   def with
